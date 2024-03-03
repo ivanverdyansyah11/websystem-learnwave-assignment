d6f1a16e23ff124391f88ebe4860c0755b0c52b5
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-03-03 20:02:53 +0800

    Progress: Finish Agenda Controller

---
+++ app/Http/Controllers/AgendaController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AgendaController extends Controller
+{
+    public function edit() {
+        return view('agenda.edit', [
+            'title' => 'Agenda Page',
+        ]);
+    }
+}
