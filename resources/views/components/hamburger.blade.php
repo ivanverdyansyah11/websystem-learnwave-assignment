0a4c89ad7504e98b0b2115ae6113b4e0a404d0c6
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-03-03 20:08:40 +0800

    Progress: Finish Hamburger Component

---
+++ resources/views/components/hamburger.blade.php
@@ -0,0 +1,3 @@
+<div class="hamburger d-flex d-lg-none justify-content-center align-items-center">
+    <img src="{{ asset('assets/images/sidebar/hamburger.png') }}" alt="Hamburger Icon" width="20" height="20">
+</div>
