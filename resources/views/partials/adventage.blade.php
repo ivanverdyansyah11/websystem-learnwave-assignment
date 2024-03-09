7e43fd9c9c4399ca6d9023f93c790cf42262a9d2
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-03-09 11:27:59 -0800

    Progress: Add New Modal Partial Adventage

---
+++ resources/views/partials/adventage.blade.php
@@ -0,0 +1,63 @@
+<div class="modal modal-adventage fade" id="adventageOneModal" tabindex="-1" aria-labelledby="adventageOneModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body">
+                <div class="modal-title d-flex align-items-center justify-content-between">
+                    <h2 class="title">Free Learning Materials</h2>
+                    <button type="button" class="button-exit" data-bs-dismiss="modal" aria-label="Close">
+                        <img src="{{ asset('assets/images/icon/exit.svg') }}" alt="Exit Icon" width="16">
+                    </button>
+                </div>
+                <p class="paragraph">At Learn Wave, we believe in democratizing education. Enjoy the advantage of access to a wealth of high-quality, free learning materials on our platform. Dive into an extensive collection of tutorials, guides, and resources without any cost, ensuring that knowledge is within reach for everyone, empowering you to embark on your C++ learning journey without financial barriers.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal modal-adventage fade" id="adventageTwoModal" tabindex="-1" aria-labelledby="adventageTwoModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body">
+                <div class="modal-title d-flex align-items-center justify-content-between">
+                    <h2 class="title">Access Anywhere</h2>
+                    <button type="button" class="button-exit" data-bs-dismiss="modal" aria-label="Close">
+                        <img src="{{ asset('assets/images/icon/exit.svg') }}" alt="Exit Icon" width="16">
+                    </button>
+                </div>
+                <p class="paragraph">Experience the unparalleled convenience of Learn Wave's C++ learning module, providing you the advantage of accessing educational content from anywhere, at any time. Break free from limitations and seamlessly integrate learning into your schedule, whether it's a quick coding session during a coffee break or a deep dive into C++ concepts from the comfort of your home. Enjoy the flexibility that fits your lifestyle and maximizes your learning potential with Learn Wave.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal modal-adventage fade" id="adventageThreeModal" tabindex="-1" aria-labelledby="adventageThreeModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body">
+                <div class="modal-title d-flex align-items-center justify-content-between">
+                    <h2 class="title">Updated Material</h2>
+                    <button type="button" class="button-exit" data-bs-dismiss="modal" aria-label="Close">
+                        <img src="{{ asset('assets/images/icon/exit.svg') }}" alt="Exit Icon" width="16">
+                    </button>
+                </div>
+                <p class="paragraph">Stay at the forefront of C++ programming with the Learn Wave advantage of continuously updated materials. Our commitment to excellence means you'll benefit from the latest industry insights, best practices, and cutting-edge content. Keep your skills sharp and relevant as our platform evolves, ensuring you're always equipped with the most up-to-date knowledge in the dynamic world of C++ development.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal modal-adventage fade" id="adventageFourModal" tabindex="-1" aria-labelledby="adventageFourModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body">
+                <div class="modal-title d-flex align-items-center justify-content-between">
+                    <h2 class="title">Mini Project Exercise</h2>
+                    <button type="button" class="button-exit" data-bs-dismiss="modal" aria-label="Close">
+                        <img src="{{ asset('assets/images/icon/exit.svg') }}" alt="Exit Icon" width="16">
+                    </button>
+                </div>
+                <p class="paragraph">Elevate your C++ proficiency with the unique Learn Wave advantage of Mini Project Exercises. Immerse yourself in hands-on learning by undertaking practical, real-world projects designed to reinforce your skills. These mini projects not only solidify theoretical concepts but also empower you to apply your knowledge in a meaningful way, fostering a deeper understanding of C++ programming through experiential learning. Gain a competitive edge in the coding landscape with our engaging and impactful Mini Project Exercises.</p>
+            </div>
+        </div>
+    </div>
+</div>
