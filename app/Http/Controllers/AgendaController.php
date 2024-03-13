ab3594e2e822178dab0534ccf8748441f73dfc2d
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-03-14 05:57:45 +0800

    Progress: Update Agenda Controller

--- app/Http/Controllers/AgendaController.php
+++ app/Http/Controllers/AgendaController.php
@@ -6,7 +6,25 @@ use Illuminate\Http\Request;
 
 class AgendaController extends Controller
 {
-    public function edit() {
+    public function index() {
+        return view('agenda.index', [
+            'title' => 'Agenda Page',
+        ]);
+    }
+
+    public function show(int $id) {
+        return view('agenda.detail', [
+            'title' => 'Agenda Page',
+        ]);
+    }
+
+    public function create() {
+        return view('agenda.create', [
+            'title' => 'Agenda Page',
+        ]);
+    }
+
+    public function edit(int $id) {
         return view('agenda.edit', [
             'title' => 'Agenda Page',
         ]);
