0961861d29398e0c390535e73938c25cbd1f0ec8
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-03-14 05:59:25 +0800

    Progress: Update Module Controller

--- app/Http/Controllers/ModuleController.php
+++ app/Http/Controllers/ModuleController.php
@@ -6,8 +6,14 @@ use Illuminate\Http\Request;
 
 class ModuleController extends Controller
 {
-    public function index() {
-        return view('module.index', [
+    // public function index() {
+    //     return view('module.index', [
+    //         'title' => 'Module Page',
+    //     ]);
+    // }
+
+    public function show(int $id) {
+        return view('module.detail', [
             'title' => 'Module Page',
         ]);
     }
